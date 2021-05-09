f9a37d2fc7cf64e2317fd3e1e2e524640b284c46
elena fouillet <elena.fouillet@epita.fr>
2021-05-09 19:12:05 +0100

    Milestones 1 : Generate the shell of an application consisting of two activities. The second activity should only be triggered when recording is stopped.

--- app/src/main/java/com/example/tracker/MainActivity.java
+++ app/src/main/java/com/example/tracker/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Buttons
+    private Button buttonEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        this.buttonEnd = findViewById(R.id.buttonEnd);
+        buttonEnd.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // go to the reportActivity
+                Intent reportActivity = new Intent(MainActivity.this,ReportActivity.class);
+
+                // start the activity
+                startActivity(reportActivity);
+            }
+        });
     }
 }
